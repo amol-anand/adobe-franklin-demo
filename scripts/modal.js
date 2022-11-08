fef1ba056de43b3f919a6f23522dc9d5e8ccfe4e
Bryan Stopp <bryan.stopp@gmail.com>
2022-11-08 14:17:27 -0600

    Hero and some glboal style fixes.

---
+++ scripts/modal.js
@@ -0,0 +1,23 @@
+(() => {
+  const displayModal = (e) => {
+    const modal = e.detail.modalEle;
+    const close = modal.querySelector('.close');
+    const backdrop = modal.querySelector('.cmp-modal--backdrop');
+
+    close.addEventListener('click', hideModal);
+    backdrop.addEventListener('click', hideModal);
+
+    modal.classList.toggle('active');
+    const video = modal.querySelector('video');
+    video.muted = false;
+    video.load();
+  }
+
+  const hideModal = () => {
+    const modal = document.querySelector('.cmp-modal.active');
+    modal.querySelector('video').pause();
+    modal.classList.toggle('active');
+  }
+
+  document.addEventListener('launchVideo', displayModal);
+})();
