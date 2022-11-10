70007b4ffa20c841dec5ddaf53d6a4d1622a56bc
Kiran Murugulla <murugull@adobe.com>
2022-11-10 15:43:48 -0500

    mobile styling , draft version

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -5,6 +5,27 @@ import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
  * @param {Element} block The footer block element
  */
 
+function hideActive(sectionLinks) {
+  const activeSections = document.querySelectorAll('.footer-links .active');
+  activeSections.forEach((activeSection) => {
+    if (activeSection !== sectionLinks) {
+      activeSection.classList.remove('active');
+    }
+  });
+}
+
+function showLinks(e) {
+  const sectionLinks = e.target.nextElementSibling;
+  hideActive(sectionLinks);
+  sectionLinks.classList.toggle('active');
+}
+
+function addLink() {
+  // hard coding for POC
+  const link = 'https://www.merative.com/contact';
+  window.open(link, '_self');
+}
+
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
@@ -12,8 +33,19 @@ export default async function decorate(block) {
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
+
+  const modifiedhtml = html.replaceAll('<h4', '<h4 class="link-section-heading"');
   const footer = document.createElement('div');
-  footer.innerHTML = html;
+
+  footer.innerHTML = modifiedhtml;
   await decorateIcons(footer);
   block.append(footer);
+  const linkHeadings = document.querySelectorAll('.footer-links .link-section-heading');
+  linkHeadings.forEach((linkHeading) => {
+    if (linkHeading.id !== 'contact-us') {
+      linkHeading.addEventListener('click', showLinks);
+    } else {
+      linkHeading.addEventListener('click', addLink);
+    }
+  });
 }
