7f6a2f81ffb9aad04d88c0956d2a1a03c5e46851
Amol Anand <amol-anand@users.noreply.github.com>
2022-11-09 21:01:21 -0500

    Base teaser-list styling done

---
+++ blocks/teaser-list/teaser-list.js
@@ -0,0 +1,35 @@
+import { lookupPages } from '../../scripts/scripts.js';
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+function createCard(row, style) {
+  const card = document.createElement('div');
+  if (style) card.classList.add(style);
+
+  const link = document.createElement('a');
+  link.classList.add('teaser-link');
+  link.href = row.path;
+
+  const title = document.createElement('div');
+  title.classList.add('teaser-title');
+  title.innerHTML = `<h3>${row.title}</h3>`;
+
+  const description = document.createElement('div');
+  description.classList.add('teaser-description');
+  description.innerHTML = `<p>${row.description}</p>`;
+
+  card.prepend(title, description, link);
+  return (card);
+}
+
+export default async function decorate(block) {
+  const pathnames = [...block.querySelectorAll('a')].map((a) => new URL(a.href).pathname);
+  block.textContent = '';
+  const pageList = await lookupPages(pathnames);
+  if (pageList.length) {
+    pageList.forEach((row) => {
+      block.append(createCard(row, 'teaser-card'));
+    });
+  } else {
+    block.remove();
+  }
+}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -41,6 +41,26 @@ function buildAutoBlocks(main) {
 }
 
 /**
+ * Gets details about pages that are indexed
+ * @param {Array} pathnames list of pathnames
+ */
+
+export async function lookupPages(pathnames) {
+  if (!window.pageIndex) {
+    const resp = await fetch(`${window.hlx.codeBasePath}/query-index.json`);
+    const json = await resp.json();
+    const lookup = {};
+    json.data.forEach((row) => {
+      lookup[row.path] = row;
+      if (row.image || row.image.startsWith('/default-meta-image.png')) row.image = `/${window.hlx.codeBasePath}${row.image}`;
+    });
+    window.pageIndex = { data: json.data, lookup };
+  }
+  const result = pathnames.map((path) => window.pageIndex.lookup[path]).filter((e) => e);
+  return (result);
+}
+
+/**
  * Decorates the main element.
  * @param {Element} main The main element
  */
