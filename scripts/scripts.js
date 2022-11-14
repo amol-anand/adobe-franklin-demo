750a91a22c4cc0c3054d35148f80ef2840cb5fa2
Amol Anand <amol@adobe.com>
2022-11-14 11:14:51 -0500

    updated based on PR comments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,12 +31,12 @@ function buildCtaBlock(main) {
   main.querySelectorAll(':scope > div').forEach((div) => {
     const h2 = div.querySelector(':scope > h2');
     const p = div.querySelector(':scope > p');
-    const a = div.querySelector(':scope p > a');
-    // eslint-disable-next-line no-bitwise
-    if (h2 && p && a && (h2.compareDocumentPosition(p) & Node.DOCUMENT_POSITION_FOLLOWING)
-          // eslint-disable-next-line no-bitwise
-          && (p.compareDocumentPosition(a) & Node.DOCUMENT_POSITION_CONTAINED_BY)) {
-      div.classList.add('cta');
+    if(p) {
+      const a = p.querySelector('a');
+      // eslint-disable-next-line no-bitwise
+      if (h2 && p && a && (h2.compareDocumentPosition(p) & Node.DOCUMENT_POSITION_FOLLOWING)) {
+        div.classList.add('cta');
+      }
     }
   });
 }
